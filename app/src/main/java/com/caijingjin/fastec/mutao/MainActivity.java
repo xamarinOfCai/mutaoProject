489a26026b15a29eaa27a5a207339b87174c1441
tangchaolizi2019 <wx_6debcb947157494093310800b296f0a7@git.code.tencent.com>
2020-08-12 21:27:29 +0800

    主页面的框架

--- app/src/main/java/com/caijingjin/fastec/mutao/MainActivity.java
+++ app/src/main/java/com/caijingjin/fastec/mutao/MainActivity.java
@@ -3,12 +3,79 @@ package com.caijingjin.fastec.mutao;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.caijingjin.fastec.mutao.fragment.FindFragment;
+import com.caijingjin.fastec.mutao.fragment.MainFragment;
+import com.caijingjin.fastec.mutao.fragment.MeFragment;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //首页
+    protected MainFragment mMainFragment = new MainFragment();
+    //发现
+    protected FindFragment mFindFragment = new FindFragment();
+    //我的
+    protected MeFragment mMeFragment = new MeFragment();
+
+
+    protected LinearLayout mMenuMain;
+    protected LinearLayout mMenuFind;
+    protected LinearLayout mMenuMe;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.getSupportFragmentManager().beginTransaction()
+                .add(R.id.container_content,mMainFragment)
+                .add(R.id.container_content,mFindFragment).hide(mFindFragment)
+                .add(R.id.container_content,mMeFragment).hide(mMeFragment)
+                .commit();
+
+        this.initViews();
+    }
+
+    private void initViews() {
+        mMenuMain =findViewById(R.id.menu_main);
+        mMenuFind =findViewById(R.id.menu_find);
+        mMenuMe =findViewById(R.id.menu_me);
+
+        mMenuMain.setOnClickListener(this);
+        mMenuFind.setOnClickListener(this);
+        mMenuMe.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.menu_main:
+                this.getSupportFragmentManager().beginTransaction()
+                        .show(mMainFragment)
+                        .hide(mFindFragment)
+                        .hide(mMeFragment)
+                        .commit();
+                break;
+            case R.id.menu_me:
+                this.getSupportFragmentManager().beginTransaction()
+                        .show(mMeFragment)
+                        .hide(mFindFragment)
+                        .hide(mMainFragment)
+                        .commit();
+                break;
+            case R.id.menu_find:
+                this.getSupportFragmentManager().beginTransaction()
+                        .show(mFindFragment)
+                        .hide(mMainFragment)
+                        .hide(mMeFragment)
+                        .commit();
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/caijingjin/fastec/mutao/fragment/FindFragment.java
@@ -0,0 +1,38 @@
+package com.caijingjin.fastec.mutao.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.caijingjin.fastec.mutao.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FindFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FindFragment extends Fragment {
+
+
+
+    public FindFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_find, container, false);
+    }
+}
